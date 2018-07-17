f64d1fe82e0c1393d6130e2ffe3407a812ce9fa0
Gabriel-Batista <gabedbatista@gmail.com>
2018-07-17 15:45:10 -0400

    mend

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,6 +2,8 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+#input: nothing
+#output: a hash of all the characters' info
 def get_all_characters
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
@@ -10,6 +12,8 @@ def get_all_characters
   character_hash["results"]
 end
 
+#input: nothing
+#output: a hash of each movie's info
 def get_all_movies
   all_movies = RestClient.get('http://www.swapi.co/api/films')
   movie_hash = JSON.parse(all_movies)
@@ -17,6 +21,8 @@ def get_all_movies
   movie_hash["results"]
 end
 
+#input: string movie name
+#output: a hash of a movie's info
 def get_movie_info (movie)
   movie_info = get_all_movies.find do |movie_hash|
     movie_hash["title"].downcase == movie
@@ -25,13 +31,16 @@ def get_movie_info (movie)
   puts "#{movie_info}"
 end
 
+#input: string character name
+#output: a hash of a character's info
 def get_character_info (character)
   get_all_characters.find do |person_hash|
     person_hash["name"].downcase == character
   end
 end
 
-
+#input: string character name
+#output: hash of film info
 
 def get_character_movies_from_api(character)
   character_info = get_character_info(character)
@@ -44,16 +53,29 @@ def get_character_movies_from_api(character)
   end
 end
 
+#input: hash of movie info
+#output: hash of movie info 
+#description: list of character's movie titles is printed to console
+
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
   films_hash.each do |film_hash|
     puts "#{film_hash["title"]}"
   end
 end
 
+#input: string character name
+#output: output of helper functions
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   unless films_hash == nil
     parse_character_movies(films_hash)
   end
 end
+
+
+
+
+
+title : A New Hope
+
+vehicles : "title or name" (https://www.swapi.co/api/vehicles/4/)
